285af861001ce34f3f54c697dac453afca5ef7b1
Jonatas <jonatas.07022003@gmail.com>
2023-06-08 16:09:41 -0300

    Criação do e html e estilização do conteúdo

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles(){
     return gulp.src('./src/style/*.scss')
@@ -7,7 +8,13 @@ function styles(){
     .pipe(gulp.dest('./dist/style'))
 }
 
-exports.default = gulp.parallel(styles)
+function images(){
+    return gulp.src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'))
+}
+
+exports.default = gulp.parallel(styles, images)
 exports.watch = function(){
     gulp.watch('./src/style/*.scss', gulp.parallel(styles))
 }
